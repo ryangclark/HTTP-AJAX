77ee3ee77a2c86c6e1b146c468bdfc91008c8ae4
Clark <rclark010@gmail.com>
2019-02-27 16:03:05 -0700

    Can now POST using the form!

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,25 +1,45 @@
 import React from 'react';
 
+import axios from 'axios';
+
+const handleFriendSubmit = event => {
+    event.preventDefault();
+    const newFriend = {
+        name: event.target[0].value,
+        age: event.target[1].value,
+        email: event.target[2].value
+    }
+    axios
+        .post(
+            'http://localhost:5000/friends',
+            newFriend
+        )//.then(response => console.log(response))
+        .catch(error => console.log(error));
+};
+
 const FriendForm = () => {
     return (
         <React.Fragment>
-            <form className="friend-form">
+            <form 
+                className="friend-form"
+                onSubmit={event => handleFriendSubmit(event)}
+            >
                 <label>
                     Name:<br></br>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" required />
                 </label>
                 <label>
                     Age:<br></br>
-                    <input type="number" name="age" />
+                    <input type="number" name="age" min="1" max="110" required />
                 </label>
                 <label>
                     Email:<br></br>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" required />
                 </label>
                 <input type="submit" value="Add Friend" />
             </form>
         </React.Fragment>
     )
-}
+};
 
 export default FriendForm;
